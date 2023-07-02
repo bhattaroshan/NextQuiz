4663187021219d313db2b50353015cefdf0a43b2
Roshan Bhatta <bhattaroshan@gmail.com>
2023-07-02 23:36:07 +0545

    added tsx

--- redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import genreReducer from './features/genre-slice';
-
-export const store = configureStore({
-    reducer:{
-        genre:genreReducer,
-    }
-});
