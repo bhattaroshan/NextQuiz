6e9112e49f384fd2af16a1e3c8c3d7cc870743c0
roshanbhatta <roshanbhatta@bottle.com.np>
2023-07-10 08:26:01 +0545

    added string

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 
 let name:number = 65;
+let test:string = "my name is roshan";
 
 name = 56;
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <>
       <p>{name}</p>
+      <p>{test}</p>
     </>
   )
 }
