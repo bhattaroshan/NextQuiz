5fbd82d3d253489f2e6ed84249bd37f10cdef2d4
roshanbhatta <bhattaroshan@gmail.com>
2024-01-30 10:21:59 +0545

    added login sample

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { Button } from '@mui/material'
+import { Button, Input, Typography } from '@mui/material'
 
 
 
@@ -7,7 +7,12 @@ import { Button } from '@mui/material'
 export default function Home() {
   return (
     <div className='flex justify-center items-center w-screen h-screen'>
-      <Button>Click Me</Button>
+      <div className='flex flex-col justify-center items-center border p-10 rounded-xl'>
+        <Typography className='font-bold mb-6'>LogIn</Typography>
+        <Input disableUnderline className='border rounded px-2'/>
+        <Button>Click Me</Button>
+
+      </div>
     </div>
   )
 }
