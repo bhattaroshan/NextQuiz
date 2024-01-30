af6fb680cd7828bf16b1b2f3520e7d0ea2c117b4
roshanbhatta <bhattaroshan@gmail.com>
2024-01-30 09:55:38 +0545

    adde material ui library

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,13 @@
 import Image from 'next/image'
+import { Button } from '@mui/material'
 
-let name:number = 66;
-let test:string = "my name is roshan";
 
-name = 56;
+
 
 export default function Home() {
   return (
-    <>
-      <p>{name}</p>
-      <p>{test}</p>
-      <p>Is there anything I can do for you?</p>
-      <p>Testing here okay!</p>
-      <p>Hero is here.</p>
-      <button>Click Me!</button>
-    </>
+    <div className='flex justify-center items-center w-screen h-screen'>
+      <Button>Click Me</Button>
+    </div>
   )
 }
