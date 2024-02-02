553a641d1d4fb5e94fa485dd413a45f4fb7c74ea
roshanbhatta <bhattaroshan@gmail.com>
2024-02-03 01:17:42 +0545

    added profile

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,9 @@ export default function Home() {
       <div className='flex flex-col justify-center items-center border p-10 rounded-xl'>
         <Typography className='font-bold mb-6'>LogIn</Typography>
         <Input disableUnderline className='border rounded px-2'/>
-        <Button variant='outlined' className='my-2'>Log In</Button>
-        <Button variant='outlined' className='my-2'>Log Out</Button>
+        <Button variant='outlined' sx={{my:'20px'}}>Log In</Button>
+        <Button variant='outlined' sx={{my:'10px'}}>Log Out</Button>
+        <Button variant='outlined' sx={{my:'10px'}}>Profile</Button>
       </div>
     </div>
   )
