c07f7e2523d6258231bf5dfcdf87849b7f840e7a
roshanbhatta <bhattaroshan@gmail.com>
2024-02-03 18:13:31 +0545

    cicd test

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,8 @@ import { Button, Input, Typography } from '@mui/material'
 
 export default function Home() {
   return (
-    <div className='flex justify-center items-center w-screen h-screen'>
+    <div className='flex flex-col justify-center items-center w-screen h-screen gap-4'>
+      <Typography className='font-bold text-xl'>Hello Everybody</Typography>
       <div className='flex flex-col justify-center items-center border p-10 rounded-xl bg-red-50'>
         <Typography className='font-bold mb-6'>LogIn</Typography>
         <Input disableUnderline className='border rounded px-2'/>
